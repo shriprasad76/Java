539832aff99b6f30eba09ce573a341eb51c0948d
shriprasad76 <shriprasadvpatil7@gmail.com>
2026-03-16 08:16:04 +0530

    committing 1st java file

---
+++ firstclass.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class firstclass{
+    public static void main(String[] args)
+    {
+        System.out.println("Hellow World");
+
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter 1st number:");
+        int a=sc.nextInt();
+        System.out.println("Enter 2nd number:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("The sum of two numbers is:"+sum);
+
+
+
+    }
+}
